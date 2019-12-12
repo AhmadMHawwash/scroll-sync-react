edda02dbd3e7abca5881019f0c22271ec9325e0f
Ahmad Hawwash <ahmad.m.hawwash@gmail.com>
2019-12-12 21:48:38 +0200

    adding example

---
+++ example/src/App.tsx
@@ -0,0 +1,186 @@
+import React, { Component } from 'react';
+
+import { ScrollSync, ScrollSyncNode } from './lib';
+
+class App extends Component {
+  render() {
+    return (
+      <ScrollSync>
+        <div style={{ display: 'flex', position: 'relative', height: 300 }}>
+          <ScrollSyncNode group="a">
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is group `a`</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+
+          <ScrollSyncNode group="a">
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is group `a`</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+
+          <ScrollSyncNode group="a">
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is group `a`</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode group="b">
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is group `b`</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode group="b">
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is group `b`</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode>
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is no group</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode>
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is no group</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode>
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is no group</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode>
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is no group</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode group="b">
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is group `b`</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode group="b">
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is group `b`</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode group="a">
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is group `a`</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode group="a">
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is group `a`</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+          <ScrollSyncNode group="a">
+            <div style={{ overflow: 'auto' }}>
+              <section style={{ height: 1000 }}>
+                <h1>This is group `a`</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam doloribus dolorum est eum eveniet
+                  exercitationem iste labore minus, neque nobis odit officiis omnis possimus quasi rerum sed soluta
+                  veritatis.
+                </p>
+              </section>
+            </div>
+          </ScrollSyncNode>
+        </div>
+      </ScrollSync>
+    );
+  }
+}
+
+export default App;
