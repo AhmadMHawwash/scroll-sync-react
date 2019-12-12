f2d05dae6f218636d780d1bbece381a557017f78
Ahmad Hawwash <ahmad.m.hawwash@gmail.com>
2019-12-12 23:51:56 +0200

    add build folder to deploy to githubio

---
+++ example/build/precache-manifest.5b6312715e25107af45f9f0f38411b97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d4be15180829e282d5434ba3c44856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "795989eb55c37e99cf10",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "a7f6fe8dc82b63ec6d44",
+    "url": "/static/js/2.eb7b07cf.chunk.js"
+  },
+  {
+    "revision": "795989eb55c37e99cf10",
+    "url": "/static/js/main.3fb9f895.chunk.js"
+  },
+  {
+    "revision": "84d6721e64a18fe343e6",
+    "url": "/static/js/runtime-main.f47b6839.js"
+  }
+]);
