81964229b38fac576f809e235e5ee81015bf5825
Ahmad Hawwash <ahmad.m.hawwash@gmail.com>
2020-06-16 14:36:23 +0300

    Adding scroll SelfLockAxis

--- example/src/build/components/ScrollSync/ScrollSyncNode.d.ts
+++ example/src/build/components/ScrollSync/ScrollSyncNode.d.ts
@@ -5,6 +5,7 @@ import React from "react";
  * Wrap your content in it to keep its scroll position in sync with other panes
  */
 export declare type ScrollConfig = "synced-only" | "syncer-only" | "two-way";
+export declare type LockAxis = "X" | "Y" | "XY" | null;
 interface ScrollSyncNodeProps {
     children: React.ReactElement;
     /**
@@ -15,6 +16,10 @@ interface ScrollSyncNodeProps {
      * if the scrolling is enabled or not
      */
     scroll?: ScrollConfig;
+    /**
+     * prevent scroll on current node if axis is locked
+     */
+    selfLockAxis?: LockAxis;
 }
 declare const ScrollSyncNode: React.ForwardRefExoticComponent<ScrollSyncNodeProps & React.RefAttributes<HTMLElement>>;
 export default ScrollSyncNode;
--- src/components/ScrollSync/ScrollSyncNode.tsx
+++ src/components/ScrollSync/ScrollSyncNode.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 /* eslint react/no-find-dom-node: 0 */
 
 import React, { FC, useContext, useEffect, useRef, forwardRef, Ref } from "react";
@@ -9,6 +10,7 @@ import { ScrollingSyncerContext } from "./ScrollSync";
  * Wrap your content in it to keep its scroll position in sync with other panes
  */
 export type ScrollConfig = "synced-only" | "syncer-only" | "two-way";
+export type LockAxis = "X" | "Y" | "XY" | null;
 
 interface ScrollSyncNodeProps {
   children: React.ReactElement;
@@ -20,31 +22,59 @@ interface ScrollSyncNodeProps {
    * if the scrolling is enabled or not
    */
   scroll?: ScrollConfig;
+  /**
+   * prevent scroll on current node if axis is locked
+   */
+  selfLockAxis?: LockAxis;
 }
 
 const toArray = (groups: string | string[]) => ([] as string[]).concat(groups);
 
+const getMovingAxis: (e: WheelEvent) => LockAxis = (e: WheelEvent) => {
+  if (e.deltaX > 0 || e.deltaX < 0) return "X";
+  if (e.deltaY > 0 || e.deltaY < 0) return "Y";
+  if ((e.deltaY > 0 || e.deltaY < 0) && (e.deltaX > 0 || e.deltaX < 0)) return "XY";
+  return null;
+};
+
 // eslint-disable-next-line react/display-name
 const ScrollSyncNode: React.ForwardRefExoticComponent<ScrollSyncNodeProps &
   React.RefAttributes<HTMLElement>> = forwardRef<HTMLElement, ScrollSyncNodeProps>(
-  ({ children, group = "default", scroll = "two-way" }, forwardedRef) => {
+  ({ children, group = "default", scroll = "two-way", selfLockAxis = null }, forwardedRef) => {
     const { registerNode, unregisterNode, onScroll } = useContext(ScrollingSyncerContext);
 
     const ref = forwardedRef || useRef<HTMLElement>(null);
 
+    const applySelfLockAxis = (event: WheelEvent) => {
+      const movingAxis = getMovingAxis(event);
+      if (selfLockAxis === "X" && movingAxis === "X") {
+        event.preventDefault();
+      } else if (selfLockAxis === "Y" && movingAxis === "Y") {
+        event.preventDefault();
+      } else if (selfLockAxis === "XY" && (movingAxis === "XY" || movingAxis === "X" || movingAxis === "Y")) {
+        event.preventDefault();
+      }
+    };
+
     useEffect(() => {
-      //@ts-ignore ref.current will difinetly exist
+      //@ts-ignore ref.current will definetly exist
       const syncableElement = { node: ref.current, scroll };
       registerNode(syncableElement, toArray(group));
+
+      //@ts-ignore
+      ref.current.onwheel = applySelfLockAxis;
+      //@ts-ignore
+      ref.current.ontouchmove = applySelfLockAxis;
+
       return () => unregisterNode(syncableElement, toArray(group));
     }, []);
 
     useEffect(() => {
       //@ts-ignore ref.current will difintly exist
       const syncableElement = { node: ref.current, scroll };
+
       unregisterNode(syncableElement, toArray(group));
       registerNode(syncableElement, toArray(group));
-      console.log(ref);
       return () => unregisterNode(syncableElement, toArray(group));
     }, [scroll, group]);
 
